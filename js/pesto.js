8bec2a2a0babcd28338eefc11ec6a7acd99529b2
Ray Chappan <rchappan@fordham.edu>
2018-12-14 14:20:25 -0500

    Initial commit

---
+++ js/pesto.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	alert("YOU'RE ABOUT TO GET HUNGRY AND IM NOT SORRY ABOUT IT!");
+
+
+	$("#pulse").onclick(function(){
+		$(".food").effect("pulsate" , {times:15} , 4000);
+		
+
+
+	});
+})
+
+		
